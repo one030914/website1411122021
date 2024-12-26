93e4d3ad1cba9f9c41e5f64b6cce90a4e9b35c3e
Llamahere <145099642+Llamahere@users.noreply.github.com>
2024-12-26 21:04:45 +0800

    20241226

--- server.js
+++ server.js
@@ -3,22 +3,66 @@
 var express = require("express");
 var server = express();
 var bodyParser = require("body-parser");
+var path = require("path");
 
 //web root
 server.use(express.static(__dirname+"/A"));
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded());
-
-var fileUpload = require("express-fileupload");
-server.use(fileUpload({defCharset:'utf8', defParamCharset:'utf8'}));
+server.use(bodyParser.urlencoded({ extended: true }));
 
 
-/*var DB = require("nedb-promises");
-var ProfolioDB = DB.create(__dirname+"/profolio.db");
-var ContactDB = DB.create(__dirname+"/contact.db");*/
- 
+var DB = require("nedb-promises");
+var WorksDB = DB.create(__dirname+"/works.db");
 
-// ProfolioDB.insert([
+
+// 檢查資料庫是否有資料，若無則初始化
+WorksDB.find({}).then(results => {
+    if (results.length === 0) {
+        // 插入初始資料
+        WorksDB.insert([
+            { modal: "#work1", imgSrc: "2024spooky month.png",text:"小馬模板-Sunshine Lollipop" },
+            { modal: "#work2", imgSrc: "cwtt32.png",text:"小馬模板-克羅利" },
+            { modal: "#work3", imgSrc: "OC復健.png",text:"小馬模板-熾燐玥" }
+        ]).then(() => {
+            console.log("初始化資料OK");
+        }).catch(err => {
+            console.error("初始化資料失敗", err);
+        });
+    } else {
+        console.log("資料庫已經有資料");
+    }
+}); 
+
+// 提供 /works 路由，讓前端能夠獲取資料庫資料
+server.get('/works', (req, res) => {  // 使用 server.get()
+    WorksDB.find({}).then(results => {
+        res.json(results);  // 返回 JSON 格式資料
+    }).catch(err => {
+        res.status(80).send('Error retrieving data from the database');
+    });
+});
+
+// 提供靜態文件 (例如 HTML, CSS, JS)
+server.use(express.static(path.join(__dirname, 'A')));
+
+
+server.listen(80, ()=>{
+    console.log("Server is running at port 80.");
+})
+
+/*
+server.get("/works", (req,res)=>{
+      //DB
+      WorksDB.find({}).then(results=>{
+        if(results != null){
+             res.send(results);
+        }else{
+            res.send("Error!");
+        }
+      })
+})*/
+
+// WorksDB.insert([
 //     { modal: "#portfolioModal1", imgSrc: "modalroundicons.png", heading: "Round Icons", text: "Graphic Design" },
 //     { modal: "#portfolioModal2", imgSrc: "startup-framework.png", heading: "Startup Framework", text: "Website Design" },
 //     { modal: "#portfolioModal3", imgSrc: "treehouse.png", heading: "Treehouse", text: "Website Design" },
@@ -26,32 +70,3 @@ var ContactDB = DB.create(__dirname+"/contact.db");*/
 //     { modal: "#portfolioModal2", imgSrc: "startup-framework.png", heading: "Startup Framework", text: "Website Design" },
 //     { modal: "#portfolioModal3", imgSrc: "treehouse.png", heading: "Treehouse", text: "Website Design" }
 // ])
-
-/*server.get("/services", (req, res)=>{
-    //DB find
-    var Services=[
-        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
-        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
-    ];
-    res.send(Services);
-});
-
-server.get("/profolio", (req,res)=>{
-      //DB
-      ProfolioDB.find({}).then(results=>{
-        if(results != null){
-             res.send(results);
-        }else{
-            res.send("Error!");
-        }
-      })
-})
-
-server.post("/contact_me", (req,res)=>{
-     ContactDB.insert(req.body);
-     res.redirect("/#contact");
-})*/
-
-server.listen(80, ()=>{
-    console.log("Server is running at port 80.");
-})
