052dcd6368b653c67b6bbe119d06e21511b7ed5d
Llamahere <145099642+Llamahere@users.noreply.github.com>
2025-01-06 18:08:18 +0800

    20240106

--- A/app.js
+++ A/app.js
@@ -1,24 +1,22 @@
-//const { createApp } = Vue;
+const { createApp } = Vue;
 
-/*const vueProfolio = createApp({
+const vueworks = createApp({
     data() {
         return {
             works: []  // 用來儲存作品資料
         };
     },
-    created() {
-        // 使用 jQuery 的 $.ajax 發送請求獲取資料
-        $.ajax({
-            url: "/works",  // 從後端 /works 路由獲取資料
-            method: "GET",
-            dataType: "json",
-            success: (results) => {
-                // 成功後將資料賦值給 Vue 實例的 works 屬性
-                this.works = results;
-            },
-            error: (error) => {
-                console.error("無法載入作品資料:", error);
-            }
-        });
+
+}).mount("#paintings")
+$.ajax({
+    url: "/works",  // 從後端 /works 路由獲取資料
+    method: "GET",
+    dataType: "json",
+    success: (results) => {
+        // 成功後將資料賦值給 Vue 實例的 works 屬性
+        vueworks.works = results;
+    },
+    error: (error) => {
+        console.error("無法載入作品資料:", error);
     }
-}).mount("#works");*/
+});
--- server.js
+++ server.js
@@ -3,54 +3,27 @@
 var express = require("express");
 var server = express();
 var bodyParser = require("body-parser");
-var path = require("path");
+
 
 //web root
-server.use(express.static(__dirname+"/A"));
+server.use(express.static(__dirname+"A"));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
 
-//var DB = require("nedb-promises");
-//var WorksDB = DB.create(__dirname+"/works.db");
+var DB = require("nedb-promises");
+var WorksDB = DB.create(__dirname+"/works.db");
 
+WorksDB.insert([
+    { modal: "#work1", imgSrc: "A/pic/OC復健.png",text:"小馬模板-Sunshine Lollipop" },
+    { modal: "#work2", imgSrc: "A/pic/克羅利.png",text:"小馬模板-克羅利" },
+    { modal: "#work3", imgSrc: "A/pic/麒麟.png",text:"小馬模板-熾燐玥" }
+]).then(() => {
+    console.log("初始化資料OK");
+}).catch(err => {
+    console.error("初始化資料失敗", err);
+});
 
-// 檢查資料庫是否有資料，若無則初始化
-/*WorksDB.find({}).then(results => {
-    if (results.length === 0) {
-        // 插入初始資料
-        WorksDB.insert([
-            { modal: "#work1", imgSrc: "2024spooky month.png",text:"小馬模板-Sunshine Lollipop" },
-            { modal: "#work2", imgSrc: "cwtt32.png",text:"小馬模板-克羅利" },
-            { modal: "#work3", imgSrc: "OC復健.png",text:"小馬模板-熾燐玥" }
-        ]).then(() => {
-            console.log("初始化資料OK");
-        }).catch(err => {
-            console.error("初始化資料失敗", err);
-        });
-    } else {
-        console.log("資料庫已經有資料");
-    }
-}); */
-
-// 提供 /works 路由，讓前端能夠獲取資料庫資料
-/*server.get('/works', (req, res) => {  // 使用 server.get()
-    WorksDB.find({}).then(results => {
-        res.json(results);  // 返回 JSON 格式資料
-    }).catch(err => {
-        res.status(80).send('Error retrieving data from the database');
-    });
-});*/
-
-// 提供靜態文件 (例如 HTML, CSS, JS)
-//server.use(express.static(path.join(__dirname, 'A')));
-
-
-server.listen(80, ()=>{
-    console.log("Server is running at port 80.");
-})
-
-/*
 server.get("/works", (req,res)=>{
       //DB
       WorksDB.find({}).then(results=>{
@@ -60,13 +33,14 @@ server.get("/works", (req,res)=>{
             res.send("Error!");
         }
       })
-})*/
+})
 
-// WorksDB.insert([
-//     { modal: "#portfolioModal1", imgSrc: "modalroundicons.png", heading: "Round Icons", text: "Graphic Design" },
-//     { modal: "#portfolioModal2", imgSrc: "startup-framework.png", heading: "Startup Framework", text: "Website Design" },
-//     { modal: "#portfolioModal3", imgSrc: "treehouse.png", heading: "Treehouse", text: "Website Design" },
-//     { modal: "#portfolioModal1", imgSrc: "roundicons.png", heading: "Round Icons", text: "Graphic Design" },
-//     { modal: "#portfolioModal2", imgSrc: "startup-framework.png", heading: "Startup Framework", text: "Website Design" },
-//     { modal: "#portfolioModal3", imgSrc: "treehouse.png", heading: "Treehouse", text: "Website Design" }
-// ])
+
+
+server.listen(3000, ()=>{
+    console.log("Server is running at port 3000.");
+})
+
+
+
+
